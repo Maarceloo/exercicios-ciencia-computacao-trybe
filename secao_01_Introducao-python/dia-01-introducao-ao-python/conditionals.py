604cae9cb396819ae5a8ec96eaff1d09e0a21af1
Marcelo De Lima <delima_marcelo@outlook.com>
2023-01-18 11:45:22 -0300

    feat exercicios 17/01/2023

---
+++ secao_01_Introducao-python/dia-01-introducao-ao-python/conditionals.py
@@ -0,0 +1,19 @@
+def check_if_word_has_4_or_more_letters(word):
+    if len(word) >= 4:
+        return True
+    else:
+        return False
+
+
+def check_what_number_is_greater(first_number, second_number):
+        return first_number if first_number > second_number else second_number
+
+def check_if_number_is_odd_or_even(number):
+    if number % 2 == 0:
+        return "even"
+    return "odd"
+     
+
+
+def check_if_element_exists_in_list(element, input_list: list):
+        return 
---
+++ secao_01_Introducao-python/dia-01-introducao-ao-python/repetition.py
@@ -0,0 +1,16 @@
+def append_each_letter_of_the_word_in_a_list(word: str):
+    # letters_list = list()
+
+    # for letter in word:
+    #     letters_list.append(letter)
+
+    # return letters_list
+
+    return [letter for letter in word]
+
+def return_index_of_the_uppercase_letter(word: str):
+    return 
+
+
+def return_element_from_list_that_is_string(input_list: list):
+    return
